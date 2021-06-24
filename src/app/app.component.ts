9e0d9408fd9dcb33c9aef94a82470ca6807774fb
daniellms1 <58314052+daniellms1@users.noreply.github.com>
2021-06-23 21:50:45 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Início', url: '/folder/Inbox', icon: 'home' },
+    { title: 'Artigos', url: '/folder/Outbox', icon: 'newspaper' },
+    { title: 'Contatos', url: '/folder/Favorites', icon: 'chatbubbles' },
+    { title: 'Sobre', url: '/folder/Archived', icon: 'information-circle' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+}
